cd7d4a4780f2ebc80f9db1dffbf65b8b26574488
Noc2 <davidhawig@gmail.com>
2026-05-12 09:26:20 +0200

    chore: remove legacy governance remediation scripts

--- test/DeploymentSecurity.t.sol
+++ test/DeploymentSecurity.t.sol
@@ -12,7 +12,6 @@ import { CompositeOracle } from "../contracts/oracles/CompositeOracle.sol";
 import { SplitRiskPoolFactory } from "../contracts/SplitRiskPoolFactory.sol";
 import { SplitRiskPool } from "../contracts/SplitRiskPool.sol";
 import { DeployYieldShieldProduction } from "../script/DeployYieldShieldProduction.s.sol";
-import { FinalizeYieldShieldProductionGovernance } from "../script/FinalizeYieldShieldProductionGovernance.s.sol";
 import { FactoryProxyTestBase } from "./helpers/FactoryProxyTestBase.sol";
 
 contract ProductionDeployHarness is DeployYieldShieldProduction {
@@ -117,43 +116,8 @@ contract DeploymentSecurityTest is Test, FactoryProxyTestBase {
         harness.validateProductionBootstrapHolder(address(contractHolder));
     }
 
-    function test_ProductionBootstrap_FinalizerRequiresPastVotesCheckpoint() public {
-        (YSToken ysToken,, YSGovernor governor) = _deployGovernance();
-        FinalizeYieldShieldProductionGovernance finalizer = new FinalizeYieldShieldProductionGovernance();
-
-        vm.prank(bootstrapHolder);
-        ysToken.delegate(bootstrapHolder);
-
-        assertGe(ysToken.getVotes(bootstrapHolder), governor.proposalThreshold());
-
-        (uint256 proposalVotes, uint256 proposalThreshold,,) =
-            finalizer.getBootstrapProposalVotes(bootstrapHolder, address(ysToken), address(governor));
-
-        assertLt(proposalVotes, proposalThreshold, "finalizer should wait for a past-vote checkpoint");
-    }
-
-    function test_ProductionBootstrap_AdminCanBeRenouncedAfterPastVotesCheckpoint() public {
-        (YSToken ysToken, TimelockController timelock, YSGovernor governor) = _deployGovernanceWithBootstrapAdmin();
-        FinalizeYieldShieldProductionGovernance finalizer = new FinalizeYieldShieldProductionGovernance();
-
-        vm.prank(bootstrapHolder);
-        ysToken.delegate(bootstrapHolder);
-        vm.warp(block.timestamp + 1);
-
-        (uint256 proposalVotes, uint256 proposalThreshold, uint256 quorumVotes,) =
-            finalizer.getBootstrapProposalVotes(bootstrapHolder, address(ysToken), address(governor));
-        assertGe(proposalVotes, proposalThreshold, "finalizer should only proceed once past votes are live");
-        assertGe(proposalVotes, quorumVotes, "finalizer should only proceed once quorum votes are live");
-
-        bytes32 adminRole = timelock.DEFAULT_ADMIN_ROLE();
-        vm.prank(bootstrapHolder);
-        timelock.renounceRole(adminRole, bootstrapHolder);
-
-        assertFalse(timelock.hasRole(adminRole, bootstrapHolder));
-    }
-
     function test_ProductionBootstrap_BurnCannotReduceBelowQuorumVotingPower() public {
-        (YSToken ysToken,, YSGovernor governor) = _deployGovernanceWithBootstrapAdmin();
+        (YSToken ysToken,, YSGovernor governor) = _deployGovernance();
         uint256 belowQuorumSupply = ysToken.MIN_GOVERNANCE_SUPPLY() - 1;
         uint256 burnAmount = ysToken.INITIAL_SUPPLY() - belowQuorumSupply;
 
@@ -194,59 +158,12 @@ contract DeploymentSecurityTest is Test, FactoryProxyTestBase {
         assertEq(erc4626OracleFeed.owner(), address(timelock));
     }
 
-    function test_LegacySepoliaTimelockAdmin_RemediationClearsOpenRolesAndDeployerAdmin() public {
-        address[] memory openAccounts = new address[](1);
-        openAccounts[0] = address(0);
-        TimelockController timelock = new TimelockController(120, openAccounts, openAccounts, deployer);
-        address governor = address(0xC0FFEE);
-
-        timelock.grantRole(timelock.PROPOSER_ROLE(), governor);
-        timelock.grantRole(timelock.EXECUTOR_ROLE(), governor);
-
-        assertTrue(timelock.hasRole(timelock.DEFAULT_ADMIN_ROLE(), deployer));
-        assertTrue(timelock.hasRole(timelock.DEFAULT_ADMIN_ROLE(), address(timelock)));
-        assertTrue(timelock.hasRole(timelock.PROPOSER_ROLE(), address(0)));
-        assertTrue(timelock.hasRole(timelock.EXECUTOR_ROLE(), address(0)));
-        assertTrue(timelock.hasRole(timelock.CANCELLER_ROLE(), address(0)));
-        assertFalse(timelock.hasRole(timelock.CANCELLER_ROLE(), governor));
-
-        timelock.grantRole(timelock.CANCELLER_ROLE(), governor);
-        timelock.revokeRole(timelock.PROPOSER_ROLE(), address(0));
-        timelock.revokeRole(timelock.EXECUTOR_ROLE(), address(0));
-        timelock.revokeRole(timelock.CANCELLER_ROLE(), address(0));
-        timelock.renounceRole(timelock.DEFAULT_ADMIN_ROLE(), deployer);
-
-        assertFalse(timelock.hasRole(timelock.DEFAULT_ADMIN_ROLE(), deployer));
-        assertFalse(timelock.hasRole(timelock.PROPOSER_ROLE(), address(0)));
-        assertFalse(timelock.hasRole(timelock.EXECUTOR_ROLE(), address(0)));
-        assertFalse(timelock.hasRole(timelock.CANCELLER_ROLE(), address(0)));
-        assertTrue(timelock.hasRole(timelock.PROPOSER_ROLE(), governor));
-        assertTrue(timelock.hasRole(timelock.EXECUTOR_ROLE(), governor));
-        assertTrue(timelock.hasRole(timelock.CANCELLER_ROLE(), governor));
-    }
-
     function _deployGovernance() internal returns (YSToken ysToken, TimelockController timelock, YSGovernor governor) {
         address[] memory emptyAccounts = new address[](0);
         timelock = new TimelockController(TIMELOCK_DELAY, emptyAccounts, emptyAccounts, deployer);
         ysToken = new YSToken(bootstrapHolder);
         governor = new YSGovernor(IVotes(address(ysToken)), timelock);
 
-        timelock.grantRole(timelock.PROPOSER_ROLE(), address(governor));
-        timelock.grantRole(timelock.EXECUTOR_ROLE(), address(governor));
-        timelock.grantRole(timelock.CANCELLER_ROLE(), address(governor));
-        timelock.renounceRole(timelock.DEFAULT_ADMIN_ROLE(), deployer);
-    }
-
-    function _deployGovernanceWithBootstrapAdmin()
-        internal
-        returns (YSToken ysToken, TimelockController timelock, YSGovernor governor)
-    {
-        address[] memory emptyAccounts = new address[](0);
-        timelock = new TimelockController(TIMELOCK_DELAY, emptyAccounts, emptyAccounts, deployer);
-        ysToken = new YSToken(bootstrapHolder);
-        governor = new YSGovernor(IVotes(address(ysToken)), timelock);
-
-        timelock.grantRole(timelock.DEFAULT_ADMIN_ROLE(), bootstrapHolder);
         timelock.grantRole(timelock.PROPOSER_ROLE(), address(governor));
         timelock.grantRole(timelock.EXECUTOR_ROLE(), address(governor));
         timelock.grantRole(timelock.CANCELLER_ROLE(), address(governor));
